f75c9abdcb03c6ca9fdd641754d65a57e32940ed
Jordan Santos <jrdnsantos@gmail.com>
2026-07-20 20:36:43 -0300

    feat: GET method part 1

--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 @Service
 public class AnimeService {
+    // private final AnimeRepository animeRepository
     public List<Anime> listAll() {
         return List.of(new Anime(1L, "Boku No Hero"), new Anime(2L, "Berserk"));
     }
